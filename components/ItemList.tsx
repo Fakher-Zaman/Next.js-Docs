3c0b91f53e40474c336348c5ee9eef443ce98144
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-05 17:51:53 +0500

    Issue Fix

--- components/ItemList.tsx
+++ components/ItemList.tsx
@@ -12,21 +12,42 @@ interface ItemListProps {
     onEdit: (item: Item) => void;
 }
 
-const ItemList: React.FC<ItemListProps> = React.memo(({ items, onRemove, onEdit }) => {
-    console.log("ItemList rendered");
-    return (
-        <ul className='list-none p-0'>
-            {items.map(item => (
-                <li key={item.id} className='flex justify-between items-center border-b p-2'>
-                    <span>{item.name} - {item.quantity}</span>
-                    <div>
-                        <button onClick={() => onEdit(item)} className='text-primary p-1 rounded mr-2'>Edit</button>
-                        <button onClick={() => onRemove(item.id)} className='text-danger p-1 rounded'>Remove</button>
-                    </div>
-                </li>
-            ))}
-        </ul>
-    );
-});
+// Component definition with a display name
+const ItemList: React.FC<ItemListProps> = React.memo(
+    ({ items, onRemove, onEdit }) => {
+        console.log('ItemList rendered');
+        return (
+            <ul className='list-none p-0'>
+                {items.map((item) => (
+                    <li
+                        key={item.id}
+                        className='flex justify-between items-center border-b p-2'
+                    >
+                        <span>
+                            {item.name} - {item.quantity}
+                        </span>
+                        <div>
+                            <button
+                                onClick={() => onEdit(item)}
+                                className='text-primary p-1 rounded mr-2'
+                            >
+                                Edit
+                            </button>
+                            <button
+                                onClick={() => onRemove(item.id)}
+                                className='text-danger p-1 rounded'
+                            >
+                                Remove
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+);
+
+// Explicitly set the display name for the memoized component
+ItemList.displayName = 'ItemList';
 
 export default ItemList;
