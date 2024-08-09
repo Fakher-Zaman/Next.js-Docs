622420f7524e4657baf7ffbcf7d118f8fba3f87b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-09 17:49:34 +0500

    next config redirection remove

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/routing/home',
-                destination: '/',
-                permanent: true,
-            },
-        ];
-    },
-};
+const nextConfig = { };
 
 export default nextConfig;
