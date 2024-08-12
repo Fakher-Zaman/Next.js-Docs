cd5f476aa09e84318e1904d2875ff710c1e78017
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-12 18:26:08 +0500

    Switcher Reload Issue Fixed at Navbar

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { VisuallyHidden, useSwitch } from "@nextui-org/react";
 import { IoMdMoon, IoMdSunny } from "react-icons/io";
 
 const ThemeSwitcher = ({ isSelected, onChange }: { isSelected: boolean; onChange: any }) => {
+    const [mode, setMode] = useState<string | null>(null);
+
     const {
         Component,
         slots,
         getBaseProps,
         getInputProps,
         getWrapperProps,
-    } = useSwitch({ isSelected, onChange });
+    } = useSwitch({ isSelected: mode === 'dark', onChange });
+
+    useEffect(() => {
+        // Get the mode from localStorage when the component mounts
+        const storedMode = localStorage.getItem('mode');
+        if (storedMode) {
+            setMode(storedMode);
+        }
+    }, [isSelected]);
 
     return (
         <div className="flex flex-col gap-2">
             <Component {...getBaseProps()} size='sm'>
                 <VisuallyHidden>
-                    {/* Ensure `isSelected` is not passed to the DOM directly */}
                     <input {...getInputProps()} />
                 </VisuallyHidden>
                 <div
@@ -28,7 +37,11 @@ const ThemeSwitcher = ({ isSelected, onChange }: { isSelected: boolean; onChange
                         ],
                     })}
                 >
-                    {isSelected ? <IoMdSunny className="text-white" /> : <IoMdMoon />}
+                    {mode === 'dark' ? (
+                        <IoMdSunny className="text-white" />
+                    ) : (
+                        <IoMdMoon />
+                    )}
                 </div>
             </Component>
         </div>
