7f5963c88a5a1b3a90ff4257a0e7ac067de87b45
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-14 18:02:46 +0500

    Dynamic Page UI

--- app/(basics)/routing/photo-feed/[id]/page.tsx
+++ app/(basics)/routing/photo-feed/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import wondersImages, { WonderImage } from "../wonders";
+import { Card, CardHeader, CardFooter, CardBody } from '@nextui-org/react';
 
 export default function PhotoPage({
   params: { id },
@@ -7,24 +8,25 @@ export default function PhotoPage({
   params: { id: string };
 }) {
   const photo: WonderImage = wondersImages.find((p) => p.id === id)!;
+
   return (
-    <div className="container mx-auto my-10">
-      <div className="w-1/2 mx-auto">
-        <div>
-          <h1 className="text-center text-3xl font-bold my-4">{photo.name}</h1>
-        </div>
-        <Image
-          alt={photo.name}
-          src={photo.src}
-          className="w-full object-cover aspect-square"
-          width={400}
-          height={400}
-        />
-        <div className="bg-white py-4">
+    <div className="container mx-auto py-4 w-[400px]">
+      <Card>
+        <CardHeader>{photo.name}</CardHeader>
+        <CardBody>
+          <Image
+            alt={photo.name}
+            src={photo.src}
+            className="w-full object-cover aspect-square"
+            width={200}
+            height={200}
+          />
+        </CardBody>
+        <CardFooter>
           <h3>{photo.photographer}</h3>
           <h3>{photo.location}</h3>
-        </div>
-      </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
