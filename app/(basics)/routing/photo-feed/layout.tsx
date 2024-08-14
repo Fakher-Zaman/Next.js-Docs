7ba24adfd757d66b2dd4c52b6d4b9e303f2dc2b7
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-14 18:08:56 +0500

    Layout Meta Data Added

--- app/(basics)/routing/photo-feed/layout.tsx
+++ app/(basics)/routing/photo-feed/layout.tsx
@@ -1,4 +1,7 @@
-import "./styles.css";
+export const metadata = {
+  title: 'Next.js Docs - Intercept Routing',
+  description: 'Exploring Next.js 14 intercepting routing features',
+};
 
 export default function Layout(props: {
   modal: React.ReactNode;
